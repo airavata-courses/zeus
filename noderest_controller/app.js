4632e12163e18ca2bdaad26bd1428ac3d365e2c4
BharathaAravind <bharathaaravind@gmail.com>
2018-09-14 20:27:38 -0400

    Updated app js

--- noderest_controller/app.js
+++ noderest_controller/app.js
@@ -47,62 +47,35 @@ app.get('/login',function(req,res){
 
 app.post('/login',function(req,res){
     
-    var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : 'root',
-        database : 'zeus_node'
-      });
-    connection.connect();
-    
     var email= req.body.uname;
     var password = req.body.psw;
-    
-    connection.query('SELECT * FROM USERTABLE WHERE EMAIL = ?',[email], function (error, results, fields) {
-    if (error) {
-        // console.log("error ocurred",error);
-        res.send({
-            "code":400,
-            "failed":"error ocurred"
-        })
-        }
-        else{
-        console.log('The solution is: ', results);
-        if(results.length >0){
-            if(results[0].PASSWORD == password){
-            res.redirect("/home");
+    request.post(
+        'http://localhost:3050/login',
+        { json: { 
+            email: req.body.uname,
+            password: req.body.psw
+         } },
+        function (error, response, body) {
+            console.log(response.body.code);
+            if (response.body.code == 200) {
+                res.redirect('/home');
             }
             else{
-            res.send({
-                "code":204,
-                "success":"Email and password does not match"
-                });
+                res.send("Incorrect login");
             }
         }
-        else{
-            res.send({
-            "code":204,
-            "success":"Email does not exits"
-                });
-        }
-    }
-    });
+    );
+   
 });
 
 app.get('/logout',function(req, res){
     console.log("test");
-    // req.session.destroy(function(){
-      res.redirect('/login');
-    // });
-  }); 
+    res.redirect('/login');
+}); 
 
-// app.post('/logout',function(req, res){
-//     console.log("test");
-//     req.session.destroy(function(){
-//       res.redirect('/login');
-//     });
-//   }); 
-
+app.get('/signup', function(req, res){
+    res.render('signup');
+});
 
 
 
@@ -110,50 +83,33 @@ app.get('/logout',function(req, res){
 
 app.post('/signup',function(req,res){
     
-    var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : 'admin',
-        database : 'zeus'
-      });
-    connection.connect();
-    
-    
-
-    var users={
-        
-        
-        "EMAIL":req.body.email,
-        "PASSWORD":req.body.password,
-        "PHONENO":req.body.PhoneNo
-        
-       
-    }
-    
-    connection.query('INSERT INTO USERTABLE SET ?',users, function (error, results, fields) {
-    if (error) {
-        // console.log("error ocurred",error);
-        res.send({
-            "code":400,
-            "failed":"error ocurred"
-        })
+    var email= req.body.uname;
+    var password = req.body.psw;
+    request.post(
+        'http://localhost:3050/signup',
+        { 
+            json: { 
+                "EMAIL":req.body.email,
+                "PASSWORD":req.body.password,
+                "PHONENO":req.body.PhoneNo
+            }
+        },
+        function (error, response, body) {
+            console.log(response.body.code);
+            if (response.body.code == 200) {
+                res.redirect('/login');
+            }
+            else{
+                res.send("Signup not successful");
+            }
         }
-        else{
-            res.render('login.ejs');        
-    }
+    );
     
-    });
 });
 
 
-app.get("/home", function(req, response){
-    var options = {
-        host: 'localhost',
-        port: 4000,
-        path: '/getVideos',
-        method: 'GET'
-    };
 
+app.get("/home", function(req, response){
     response.render('home')
 });
 
