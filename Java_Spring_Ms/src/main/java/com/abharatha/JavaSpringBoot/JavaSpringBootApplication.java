65cdcd557fb375e76ee3bcb2952b739cbafbea96
BharathaAravind <bharathaaravind@gmail.com>
2018-09-16 19:31:58 -0400

    Adding Unit tests

--- Java_Spring_Ms/src/main/java/com/abharatha/JavaSpringBoot/JavaSpringBootApplication.java
+++ Java_Spring_Ms/src/main/java/com/abharatha/JavaSpringBoot/JavaSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.abharatha.JavaSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.abharatha.JavaSpringBoot.dao")
 public class JavaSpringBootApplication {
 
 	public static void main(String[] args) {
