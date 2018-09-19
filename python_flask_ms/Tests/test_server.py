319b25b07a93f9704d320bcd9b5d6e236023e44e
shivteja14 <shivteja14@gmail.com>
2018-09-19 19:33:21 -0400

    Added Travis and unit tests

---
+++ python_flask_ms/Tests/test_server.py
@@ -0,0 +1,28 @@
+import unittest
+from app import server
+
+class MyTestClass(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+
+    def setUp(self):
+        self.app = server.python_flask_ms().test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_python_flask_ms_server(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ python_flask_ms/app/server.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import request, jsonify
+from flaskext.mysql import MySQL
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+
+def python_flask_ms():
+    CORS(app)
+    mysql = MySQL()
+    app.config['MYSQL_DATABASE_USER']='aravind'
+    app.config['MYSQL_DATABASE_PASSWORD']='aravind'
+    app.config['MYSQL_DATABASE_DB']='zeus_flask'
+    app.config['MYSQL_DATABASE_HOST']='localhost'
+
+
+    @app.route('/', methods = ['POST', 'GET'])
+    def route():
+        if request.method=='GET':
+            return "hello world"
+        else:
+            return "post req"
+
+
+
+    @app.route("/getVideos", methods = ['GET'])
+    def getVideos():
+        cursor = mysql.connect().cursor()
+        cursor.execute("SELECT * from VideoTable")
+        data = cursor.fetchall()
+        return jsonify(data)
+
+
+
+    mysql.init_app(app)
+
+    return app
+
+
+if __name__ == '__main__':
+   app = python_flask_ms()
+   app.run("localhost","4000")
+
+
+
+
