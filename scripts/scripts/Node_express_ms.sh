4f7b98f7faaca88ab32d161684a6b0759784ef93
BharathaAravind <bharathaaravind@gmail.com>
2018-10-14 21:15:15 -0400

    Update Node_express_ms.sh

--- scripts/scripts/Node_express_ms.sh
+++ scripts/scripts/Node_express_ms.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../CompleteProject/NodeExpressMS/zeus/node_express_ms
 sudo npm install
-sudo npm install forever -g
+sudo npm install forever -g || true
 sudo forever start node app.js &
