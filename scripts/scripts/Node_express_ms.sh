846a8ece7bf9b6a15c48a6598f50b87cc21bf7a7
Aravind Bharatha <bharathaaravind@gmail.com>
2018-10-14 22:36:06 -0400

    updated node scripts

--- scripts/scripts/Node_express_ms.sh
+++ scripts/scripts/Node_express_ms.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cd ../CompleteProject/NodeExpressMS/zeus/node_express_ms
 sudo npm install
-sudo npm install forever -g || true
 sudo forever start node app.js &
--- scripts/scripts/Node_rest_controller.sh
+++ scripts/scripts/Node_rest_controller.sh
@@ -3,5 +3,4 @@
 
 cd ../CompleteProject/NodeRestController/zeus/noderest_controller
 sudo npm install
-sudo npm install forever -g || true
 sudo forever start node app.js &
