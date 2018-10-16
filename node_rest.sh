440e8b354d93ee81831a755bf0983f64069c5f6c
root <root@js-168-221.jetstream-cloud.org>
2018-10-15 20:39:42 -0400

    Updating deploy script

--- node_rest.sh
+++ node_rest.sh
@@ -3,4 +3,4 @@ cd noderest_controller
 docker rm -f node_rest_ms_cont
 docker rmi node_rest_ms
 docker build -t node_rest_ms .
-docker run -d --net="host" -p=3001 --name java_rest_ms_cont node_rest_ms
+docker run -d --net="host" -p=3001 --name node_rest_ms_cont node_rest_ms
