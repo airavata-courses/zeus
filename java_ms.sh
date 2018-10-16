1488740c64816d9087be8c98818f1ab9af814eaf
root <root@js-168-221.jetstream-cloud.org>
2018-10-15 20:00:04 -0400

    Adding Docker file and shell file to deploy

---
+++ java_ms.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd Java_Spring_Ms
+docker rm -f java_ms_cont
+docker rmi java_ms
+mvn clean install
+docker build -t java_ms .
+docker run -d --net="host" -p=8090 --name java_ms_cont java_ms
