a7c46f642141222d85329ca02dbd655c8bd6d3b6
harithadamarla <haritha.cbit2010@gmail.com>
2018-11-29 19:29:45 -0500

    Update java_spring.sh

--- scripts/scripts/java_spring.sh
+++ scripts/scripts/java_spring.sh
@@ -2,7 +2,7 @@
 cd ../CompleteProject/SpringMSApplication/zeus/Java_Spring_Ms
 docker rm -f java_ms_cont
 docker rmi java_ms
-mvn clean install
+mvn clean install -DskipTests=true
 docker build -t java_ms .
 docker run -d --net="host" -p=8090 --name java_ms_cont java_ms
 
