7f38e797ed9ffbc87c703766d08ea4353cbf8b33
Aravind Bharatha <bharathaaravind@gmail.com>
2018-12-06 12:34:56 -0500

    checking flow

--- python_flask_ms/app/server.py
+++ python_flask_ms/app/server.py
@@ -84,7 +84,7 @@ def python_flask_ms():
         # app.logger.info('Doing Something')
 
         if request.method=='GET':
-            return "hello world. testing CI/CD workflow. Will it work>"
+            return "Testing CI/CD workflow. Will it work?"
         else:
             return "post req"
 
