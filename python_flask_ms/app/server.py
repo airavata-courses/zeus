9c52e722606d015d2eef1fdf8e37f019f5836576
Haritha Damarla <haritha.cbit2010@gmail.com>
2018-12-07 22:21:30 -0500

    For Demo

--- python_flask_ms/app/server.py
+++ python_flask_ms/app/server.py
@@ -84,7 +84,7 @@ def python_flask_ms():
         # app.logger.info('Doing Something')
 
         if request.method=='GET':
-            return "For CI/CD Demo"
+            return "For CI/CD Demo- Dec 7th"
         else:
             return "post req"
 
